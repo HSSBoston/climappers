6b0511e3dbc871bd21fffc1fee7232b517c5e3db
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-10-20 09:45:30 -0400

    wbgt-music added

---
+++ code/wbgt-classification/wbgt.py
@@ -0,0 +1,10 @@
+from noaa_wbgt import getWbgt
+
+lat = 42.490027972375344
+lon = -71.28465073182961
+
+currentWbgt, timeToWbgtDictToday, timeToWbgtDictTomorrow, timeToWbgtDictWeek = getWbgt(lat, lon)
+
+print("Current WBGT (F): ", currentWbgt)
+print(timeToWbgtDictToday)
+
